c8e7907efe41047baa60c2c594356b4d1529b4e0
Greg Schneider <greg53x@msn.com>
2022-05-30 18:24:25 -0600

    logic for revealing hidden select elements

--- js.js
+++ js.js
@@ -2,3 +2,10 @@ const a2022530 = {
     squat:{weight:'215',reps:['5','5','5','5','5']},
     ohp:{weight:'55',reps:['16','16','16','16','16']},
 };
+function testFunction(){
+    if (movement.value == 'squat') {
+        document.getElementById('squatOptions').style.visibility = 'visible';
+    }
+}
+const movement = document.getElementById('movement');
+movement.addEventListener('change',testFunction);
