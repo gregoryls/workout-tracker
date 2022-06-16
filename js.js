651cf700e89e135df6fe0f6b66149367f2dcd067
Greg Schneider <greg53x@msn.com>
2022-06-15 20:36:23 -0600

    comments for future planning

--- js.js
+++ js.js
@@ -9,3 +9,7 @@ function testFunction(){
 }
 const movement = document.getElementById('movement');
 movement.addEventListener('change',testFunction);
+
+//big list of exercise full names, e.g. flat barbell bench, incline dumbell bench etc.
+//maybe select options to narrow down a populated list of every exercise
+//which is also filterable by a search field.
