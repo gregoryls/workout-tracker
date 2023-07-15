7ba030d2e4be42ea24cbe2d7ce3cac464d8ac904
Greg Schneider <greg53x@msn.com>
2023-07-14 23:52:55 -0600

    remove unused files

--- src/index.js
+++ src/index.js
@@ -13,3 +13,28 @@ movement.addEventListener("change", testFunction);
 // big list of exercise full names, e.g. flat barbell bench, incline dumbell bench etc.
 // maybe select options to narrow down a populated list of every exercise
 // which is also filterable by a search field.
+
+workoutData = {};
+workoutData["day"] = {
+  date: new Date(2022, 06, 20, 2, 45, 0),
+  workoutNumber: 1,
+  exercises: {
+    excercise1: {
+      name: "squat",
+      set1: {
+        weight: 225,
+        reps: 5,
+      },
+      set2: {
+        weight: 185,
+        reps: 8,
+      },
+    },
+    excercise2: {
+      name: "bench",
+      155: 10,
+      145: 15,
+    },
+  },
+};
+console.log(Object.keys(workoutData.day.exercises.excercise1)[0]);
