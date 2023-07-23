cfef1987312a04074020c32073d7f3f046e1f60d
Greg Schneider <greg53x@msn.com>
2023-07-22 23:34:39 -0600

    expand camelCase function to include - and _ cases

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export function toCamelCase(string) {
   // arrow function uses throwaway variable for the regex match,
   // capture group 0, as per replace() format
-  return string.replace(/[\s](.)/g, (_, char) => char.toUpperCase());
+  return string.replace(/[-_\s](.)/g, (_, char) => char.toUpperCase());
 }
 export function placeholder() {}
