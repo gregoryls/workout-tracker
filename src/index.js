1b034d9f538466e8d1888e8c191466d05236bf82
Greg Schneider <greg53x@msn.com>
2023-08-01 22:59:12 -0600

    prevent addition of empty string to exercise list obj

--- src/index.js
+++ src/index.js
@@ -58,7 +58,10 @@ exerciseInput.addEventListener("input", () => {
 
 submitExercise.addEventListener("click", () => {
   const value = utils.getMovementInput();
-  console.log(utils.getInputValue("rpe"));
+  // console.log(utils.getInputValue("rpe"));
+
+  // don't add null entry to exercise list object
+  if (value === "") return;
 
   // const exerciseOptions = exerciseListElement.options;
   const valueCamelCase = utils.toCamelCase(value.toLowerCase());
