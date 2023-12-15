30849f1523ed788cd5223e36a70fdb0bc4084122
Greg Schneider <greg53x@msn.com>
2023-12-14 23:00:22 -0700

    remove old workoutData object

--- src/index.js
+++ src/index.js
@@ -281,27 +281,3 @@ clear.addEventListener("click", () => {
   localStorage.clear();
   location.reload();
 });
-
-const workoutData = {};
-workoutData.day = {
-  date: new Date(2022, 0o6, 20, 2, 45, 0),
-  workoutNumber: 1,
-  exercises: {
-    excercise1: {
-      name: "squat",
-      set1: {
-        weight: 225,
-        reps: 5,
-      },
-      set2: {
-        weight: 185,
-        reps: 8,
-      },
-    },
-    excercise2: {
-      name: "bench",
-      155: 10,
-      145: 15,
-    },
-  },
-};
