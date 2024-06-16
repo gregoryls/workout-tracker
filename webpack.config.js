97a32b33a9a81c1d4f6ea36fd9ca263faf66a920
Greg Schneider <greg53x@msn.com>
2024-06-16 16:16:55 -0600

    fix extraneous webpack entry points

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
-    exercises: "./src/exercises.js",
-    utils: "./src/utils.js",
+
     // module1: "./src/module1.js",
   },
   devtool: "inline-source-map",
