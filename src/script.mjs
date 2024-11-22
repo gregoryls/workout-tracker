9b37fb9bc25fe371f9c64b9db1b02b84d4f996b5
Greg Schneider <greg53x@msn.com>
2024-11-21 23:18:22 -0700

    async node function to update json file

--- src/script.mjs
+++ src/script.mjs
@@ -1,5 +1,23 @@
 import fs from "fs";
 import exercises from "./exercises.json" with { type: "json" };
-import mesocycleAdditions from "./mesocycleAdditions.json" with { type: "json" };
+// import mesocycleAdditions from "./mesocycleAdditions.json" with { type: "json" };
 
 console.log(mesocycleAdditions);
+
+export async function updateJson(filePath, newData) {
+  try {
+    // get existing file
+    const fileData = await fs.readFile(filePath, "utf8", propertyToUpdate);
+    const jsonData = JSON.parse(fileData);
+
+    // update new data
+    jsonData.propertyToUpdate.push(newData);
+
+    // write to file
+    await fs.writeFile(filePath, JSON.stringify(jsonData, null, 4));
+    console.log("json updated");
+  } catch (error) {
+    console.error("Error updating JSON: ", error);
+    throw error;
+  }
+}
