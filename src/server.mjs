2925c1e09be51d2dd6281aa7cb19358cd22916e7
Greg Schneider <greg53x@msn.com>
2024-11-22 23:20:39 -0700

    node server to use updateJson script

--- src/server.mjs
+++ src/server.mjs
@@ -1,5 +1,24 @@
 import express from "express";
-import { execFile } from "child_process";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
+import { updateJson } from "./script.mjs";
+
+const app = express();
+const PORT = 3000;
+const mesocycleFilePath = "./mesocycleAdditions.json";
+
+app.use(express.json());
+
+// endpoint
+app.post("/process", async (req, res) => {
+  try {
+    const userData = req.body;
+    await updateJson(mesocycleFilePath, userData);
+    res.json({ message: "Data processed successfully!", data: userData });
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred." });
+  }
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
